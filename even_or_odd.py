c9dbbbf7161866cd20083ba4059dfc0ec0900c17
DEEPAK P <nanideepak05@gmail.com>
2025-12-08 23:22:18 +0530

    even_or_odd

--- even_or_odd.py
+++ even_or_odd.py
@@ -6,4 +6,25 @@ if __name__ == "__main__":
         print(f"{num} is even")
     else:
         print(f"{num} is odd")
-        
+#this is with using modulus operator
+
+#with simple if else
+
+num = int(input("enter the number:"))        
+if num % 2 ==0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
+    
+#with using function
+
+def even(n):
+    if n % 2 == 0:
+        return "even"
+    else:
+        return "odd"
+num = int(input("enter the number:"))
+print(even(num))
+    
+
+
