2e9e3b4384e3b98e21e2414bbe83ff815ed69cfe
DEEPAK P <nanideepak05@gmail.com>
2025-12-11 22:39:33 +0530

    reverse from n to 1

--- reverse_n_to_1.py
+++ reverse_n_to_1.py
@@ -1,12 +1,8 @@
 def display(n:int)->None:
-    i = 1
-    while i<= n:
-        print(i, end= " ")
-        i =i+1
-
+    i = n
+    while i > 0:
+        print(i, end=" ")
+        i =i-1
 if __name__ == '__main__':
     n = int(input("enter the number:"))
     display(n)
-    
-
-# 
