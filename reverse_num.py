f5d93556f2866a1dad73918ae6f654d88fbc4b07
DEEPAK P <nanideepak05@gmail.com>
2025-12-14 17:18:48 +0530

    reverse the numbers

---
+++ reverse_num.py
@@ -0,0 +1,11 @@
+def rev_number(n:int)->int:
+    rev = 0
+    while n!=0:
+        d = n % 10
+        rev = d + (rev * 10)
+        n = n //10
+    return rev
+
+if __name__ == '__main__':
+    n = int(input("enter the number:"))
+    print(rev_number(n))
