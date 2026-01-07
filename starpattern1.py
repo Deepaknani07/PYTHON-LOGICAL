a765922ba835b1aaa0ec99f5651637bedaa26243
DEEPAK P <nanideepak05@gmail.com>
2026-01-07 22:17:09 +0530

    star

---
+++ starpattern1.py
@@ -0,0 +1,22 @@
+# size = 10
+# for i in range(size):
+#     print("*"*(i+1))
+
+
+# size = 10
+# for i in range(size):
+#     print("*"*size)
+
+# size = 10
+# for i in range(size): # right 
+#     count = size-1
+#     print("*"*i)
+    
+    
+size = 10
+for i in range(size): # right 
+    count = size-1
+    
+    print("*"*count)
+    print("*"*(i+1))
+    
